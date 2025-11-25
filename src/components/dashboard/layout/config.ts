9045879b1e8710b1ee1d6c7ba5b80a2abe936c67
mahdiboubtananazi-cloud <mahdiboubtananazi@gmail.com>
2025-11-25 20:51:42 +0100

    Ready for Vercel Deployment

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -1,11 +1,9 @@
 import type { NavItemConfig } from '@/types/nav';
-import { paths } from '@/paths';
 
 export const navItems = [
-  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
-  { key: 'customers', title: 'Customers', href: paths.dashboard.customers, icon: 'users' },
-  { key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
-  { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
-  { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
-  { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
+  { key: 'dashboard', title: '???? ??????', href: '/dashboard', icon: 'chart-pie' },
+  { key: 'home', title: '????????', href: '/', icon: 'user' },
+  { key: 'archive', title: '????? ?????????', href: '/archive', icon: 'users' },
+  { key: 'add', title: '????? ????', href: '/add-parasite', icon: 'plugs-connected' },
+  { key: 'settings', title: '?????????', href: '/dashboard', icon: 'gear-six' }
 ] satisfies NavItemConfig[];
